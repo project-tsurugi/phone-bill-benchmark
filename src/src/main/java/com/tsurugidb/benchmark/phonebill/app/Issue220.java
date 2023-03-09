25c04106f1e69a318f9f89efeb27fecedfe1ed42
Shinichi Umegane <sumegane@nifty.com>
2023-03-09 17:46:56 +0900

    Add logging for each transaction

--- src/src/main/java/com/tsurugidb/benchmark/phonebill/app/Issue220.java
+++ src/src/main/java/com/tsurugidb/benchmark/phonebill/app/Issue220.java
@@ -1,5 +1,7 @@
 package com.tsurugidb.benchmark.phonebill.app;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,16 +20,18 @@ import com.tsurugidb.benchmark.phonebill.db.PhoneBillDbManager;
 import com.tsurugidb.benchmark.phonebill.db.TxLabel;
 import com.tsurugidb.benchmark.phonebill.db.TxOption;
 import com.tsurugidb.benchmark.phonebill.db.TxOption.Table;
+import com.tsurugidb.benchmark.phonebill.db.dao.ContractDao;
 import com.tsurugidb.benchmark.phonebill.db.dao.Ddl;
 import com.tsurugidb.benchmark.phonebill.db.dao.HistoryDao;
+import com.tsurugidb.benchmark.phonebill.db.entity.Contract;
 import com.tsurugidb.benchmark.phonebill.db.entity.History;
 import com.tsurugidb.benchmark.phonebill.testdata.PhoneNumberGenerator;
 
 public class Issue220 extends ExecutableCommand {
     private static final Logger LOG = LoggerFactory.getLogger(Issue220.class);
 
-
-	private static final int[] THREAD_COUNTS = { 64, 16, 4, 1 };
+	private static boolean LOGGING_DETAIL_TIME_INFO = true;
+	private static final int[] THREAD_COUNTS = { 64, 16 };
 	private static final TxOption OCC  =  TxOption.ofOCC(1, TxLabel.TEST);
 	private static final TxOption LTX = TxOption.ofLTX(1, TxLabel.TEST, Table.HISTORY);
 
@@ -35,6 +39,7 @@ public class Issue220 extends ExecutableCommand {
 	private AtomicInteger retryCounter;
 	private static int recordsPerCommit;
 	private List<Record> records = new ArrayList<>();
+	private static String threadLabel = "";
 
 
 	public static void main(String[] args) throws Exception {
@@ -56,28 +61,30 @@ public class Issue220 extends ExecutableCommand {
 			manager.execute(TxOption.of(), () -> {
 				ddl.dropTables();
 				ddl.createHistoryTable();
+				ddl.createContractsTable();
 			});
 		}
 
 
 
 		// スレッド数、TxOptionを変えて複数回実行する
-		for (int threadCount : THREAD_COUNTS) {
+		for (int i = 0; i < 1; i++) {
+			for (int threadCount : THREAD_COUNTS) {
+				threadLabel = String.format("T%02d", threadCount);
 			execute(config, threadCount, () -> {
 				return new InsertTask(config, OCC, false);
 			}, "Insert, OCC");
 			execute(config, threadCount, () -> {
 				return new DeleteTask(config, OCC, false);
 			}, "Delete, OCC");
-			execute(config, threadCount, () -> {
-				return new InsertTask(config, LTX, false);
-			}, "Insert, LTX");
-			execute(config, threadCount, () -> {
-				return new DeleteTask(config, LTX, false);
-			}, "Delete, LTX");
+				execute(config, threadCount, () -> {
+					return new InsertTask(config, LTX, false);
+				}, "Insert, LTX");
+				execute(config, threadCount, () -> {
+					return new DeleteTask(config, LTX, false);
+				}, "Delete, LTX");
+			}
 		}
-
-
 	}
 
 	public void execute(Config config, int threadCount, Supplier<Runnable> supplier, String description)
@@ -85,14 +92,18 @@ public class Issue220 extends ExecutableCommand {
 
 
 		Record record = new Record(description + ", T" + String.format("%02d", threadCount));
-		record.start();
-		records.add(record);
 		remaining = new AtomicLong(config.numberOfHistoryRecords);
 		retryCounter = new AtomicInteger(0);
 		ExecutorService service = Executors.newFixedThreadPool(threadCount);
 		List<Future<?>> futures = new ArrayList<>(threadCount);
+		List<Runnable> tasks = new ArrayList<>(threadCount);
 		for (int i = 0; i < threadCount; i++) {
-			Future<?> f = service.submit(supplier.get());
+			tasks.add(supplier.get());
+		}
+		record.start();
+		records.add(record);
+		for (int i = 0; i < threadCount; i++) {
+			Future<?> f = service.submit(tasks.get(i));
 			futures.add(f);
 		}
 		TateyamaWatcher tateyamaWatcher = new TateyamaWatcher();
@@ -113,6 +124,8 @@ public class Issue220 extends ExecutableCommand {
 			});
 			LOG.info("History table has {} records.", c);
 		}
+
+		PhoneBillDbManager.reportNotClosed();
 		// レポート出力
 		System.out.println("Type, Option,Threads ,NumberOfRecords , Time(sec), RetryCount, VSZ, RSS");
 		records.sort((r1, r2) -> r1.description.compareTo(r2.description));
@@ -127,11 +140,20 @@ public class Issue220 extends ExecutableCommand {
 		private Config config;
 		private TxOption txOption;
 		private boolean skipDbAccess;
+		private PhoneBillDbManager manager;
+
 
 		public InsertTask(Config config, TxOption txOption, boolean skipDbAccess) {
 			this.config = config;
 			this.txOption = txOption;
 			this.skipDbAccess = skipDbAccess;
+			manager = PhoneBillDbManager.createPhoneBillDbManager(config);
+			ContractDao dao =  manager.getContractDao();
+			manager.execute(OCC, () -> {
+				List<Contract>list = dao.getContracts();
+				LOG.debug("contract has {} records.", list.size());
+			});
+
 		}
 
 		@Override
@@ -141,8 +163,9 @@ public class Issue220 extends ExecutableCommand {
 
 			PhoneNumberGenerator phoneNumberGenerator = new PhoneNumberGenerator(config);
 
-			try (PhoneBillDbManager manager = PhoneBillDbManager.createPhoneBillDbManager(config)) {
+			try  {
 				HistoryDao dao  = manager.getHistoryDao();
+				Timer timer = new Timer("INSERT", txOption == LTX ? "LTX" : "OCC");
 				for (;;) {
 					long basePhoneNumber = remaining.getAndAdd(-recordsPerCommit);
 					if (basePhoneNumber <= 0) {
@@ -165,8 +188,12 @@ public class Issue220 extends ExecutableCommand {
 						for (;;) {
 							try {
 								manager.execute(txOption, () -> {
+									timer.setStartTx();
 									dao.batchInsert(list);
+									timer.setStartCommit();
+
 								});
+								timer.setEndCommit();
 								break;
 							} catch (RuntimeException e) {
 								LOG.debug("Fail to insert", e.getMessage());
@@ -177,8 +204,10 @@ public class Issue220 extends ExecutableCommand {
 					counter += n;
 					LOG.debug("{} records inserted.", counter);
 				}
+			} finally {
+				manager.close();
 			}
-			LOG.debug("End insert task({} records inserted, retryCounter = {}).", counter, retryCounter.get());
+//			LOG.debug("End insert task({} records inserted, retryCounter = {}).", counter, retryCounter.get());
 		}
 	}
 
@@ -186,11 +215,18 @@ public class Issue220 extends ExecutableCommand {
 		private Config config;
 		private TxOption txOption;
 		private boolean skipDbAccess;
+		private PhoneBillDbManager manager;
 
 		public DeleteTask(Config config, TxOption txOption, boolean skipDbAccess) {
 			this.config = config;
 			this.txOption = txOption;
 			this.skipDbAccess = skipDbAccess;
+			manager = PhoneBillDbManager.createPhoneBillDbManager(config);
+			ContractDao dao =  manager.getContractDao();
+			manager.execute(OCC, () -> {
+				List<Contract>list = dao.getContracts();
+				LOG.debug("contract has {} records.", list.size());
+			});
 		}
 
 		@Override
@@ -200,8 +236,9 @@ public class Issue220 extends ExecutableCommand {
 
 			PhoneNumberGenerator phoneNumberGenerator = new PhoneNumberGenerator(config);
 
-			try (PhoneBillDbManager manager = PhoneBillDbManager.createPhoneBillDbManager(config)) {
+			try  {
 				HistoryDao dao  = manager.getHistoryDao();
+				Timer timer = new Timer("DELETE", txOption == LTX ? "LTX" : "OCC");
 				for (;;) {
 					long basePhoneNumber = remaining.getAndAdd(-recordsPerCommit);
 					if (basePhoneNumber <= 0) {
@@ -216,10 +253,13 @@ public class Issue220 extends ExecutableCommand {
 						for (;;) {
 							try {
 								manager.execute(txOption, () -> {
+									timer.setStartTx();
 									for (String phoneNumber : list) {
 										dao.delete(phoneNumber);
 									}
+									timer.setStartCommit();
 								});
+								timer.setEndCommit();
 								break;
 							} catch (RuntimeException e) {
 								LOG.debug("Fail to delete", e.getMessage());
@@ -228,8 +268,10 @@ public class Issue220 extends ExecutableCommand {
 						}
 					}
 					counter += n;
-					LOG.debug("{} records deleteed.", counter);
+//					LOG.debug("{} records deleteed.", counter);
 				}
+			} finally {
+				manager.close();
 			}
 			LOG.debug("End delete task({} records deleteed, retryCounter = {}).", counter, retryCounter.get());
 		}
@@ -262,15 +304,61 @@ public class Issue220 extends ExecutableCommand {
 			rss = convertToGB(tateyamaWatcher.getRss());
 		}
 
-
 		void print() {
 			System.out.println(description + ", " + numberOfRecords + ", " + elapsedMillis / 1000.0 + "," + retryCount
 					+ ", " + vsz + ", " + rss);
 		}
+
+		private String convertToGB(long bytes) {
+		    double gb = (double) bytes / (1024 * 1024 * 1024);
+		    return String.format("%.1fGB", gb);
+		}
 	}
 
-	private static String convertToGB(long bytes) {
-	    double gb = (double) bytes / (1024 * 1024 * 1024);
-	    return String.format("%.1fGB", gb);
+
+	private static class Timer {
+		String queryType;
+		String txOption;
+
+		Instant startTx = null;
+		Instant startCommit = null;
+		Instant endCommit = null;
+
+		public Timer(String queryType, String txOption) {
+			this.queryType = queryType;
+			this.txOption = txOption;
+		}
+
+		public void setStartTx() {
+			if (LOGGING_DETAIL_TIME_INFO) {
+//				LOG.debug("setStartTx called");
+				startTx = Instant.now();
+			}
+		}
+
+		public void setStartCommit() {
+			if (LOGGING_DETAIL_TIME_INFO) {
+//				LOG.debug("setStartCommit called");
+				startCommit = Instant.now();
+			}
+		}
+
+		public void setEndCommit() {
+			if (LOGGING_DETAIL_TIME_INFO) {
+//				LOG.debug("setEndCommit called");
+				endCommit = Instant.now();
+				writeLog();
+			}
+		}
+
+		private void writeLog() {
+			if (LOGGING_DETAIL_TIME_INFO) {
+				Duration d1 = Duration.between(startTx, startCommit);
+				Duration d2 = Duration.between(startCommit, endCommit);
+				LOG.debug("TIME INFO: {}\t{}\t{}\t{}\t{}", threadLabel, queryType, txOption,
+						d1.toSeconds() * 1000 * 1000 + d1.toNanos() / 1000,  	// Durationをマイクロ秒で表示
+						d2.toSeconds() * 1000 * 1000 + d2.toNanos() / 1000);	// Durationをマイクロ秒で表示
+			}
+		}
 	}
 }
