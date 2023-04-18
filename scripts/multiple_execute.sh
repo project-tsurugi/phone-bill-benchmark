7c19d426661471c0d1a54ceefc59ce521945382a
Shinichi Umegane <sumegane@nifty.com>
2023-04-18 15:20:04 +0900

    Change symbolic links created during multiple_execute.sh execution to relative paths

--- scripts/multiple_execute.sh
+++ scripts/multiple_execute.sh
@@ -22,9 +22,9 @@ export JAVA_OPTS="$JAVA_OPTS -Dcom.tsurugidb.tsubakuro.jniverify=false \
  -Dlog.detail=$LOG_DIR/detail-$ext \
  -Dlog.detail.online=$LOG_DIR/detail-online-$ext"
 
-ln -sf $LOG_DIR/summary-$ext $LOG_DIR/summary.log
-ln -sf $LOG_DIR/detail-$ext $LOG_DIR/detail.log
-ln -sf $LOG_DIR/detail-online-$ext $LOG_DIR/detail-online.log
+ln -sf summary-$ext $LOG_DIR/summary.log
+ln -sf detail-$ext $LOG_DIR/detail.log
+ln -sf detail-online-$ext $LOG_DIR/detail-online.log
 
 $INSTALL_DIR/phone-bill/bin/run MultipleExecute "$@"
 
