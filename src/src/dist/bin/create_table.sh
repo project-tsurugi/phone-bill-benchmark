416feaf71d37b799a5d552d946594e32a6bc0325
Shinichi Umegane <sumegane@nifty.com>
2023-06-15 16:57:53 +0900

    Add script to tarball

---
+++ src/src/dist/bin/create_table.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ue
+
+if [ $# -eq 0 ]; then
+  echo usage: $0 config_files
+  exit 1
+fi
+
+if [[ -z "$TSURUGI_HOME" ]]; then
+  echo "Error: The environment variable TSURUGI_HOME is not set. Please set TSURUGI_HOME."
+fi
+
+BIN_DIR=$(cd $(dirname $0); pwd)
+
+ext=`date "+%Y%m%d-%H%M%S"`.log
+export JAVA_OPTS="$JAVA_OPTS -Dcom.tsurugidb.tsubakuro.jniverify=false \
+ -Dlogback.configurationFile=$BIN_DIR/../etc/logback.xml \
+ -Dlogfile=$BIN_DIR/../logs/create_test_data-$ext"
+
+$BIN_DIR/run CreateTable "$@"
+
