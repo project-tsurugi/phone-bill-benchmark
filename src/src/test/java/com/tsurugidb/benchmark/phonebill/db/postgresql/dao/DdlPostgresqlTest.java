d50e744f2f388c432d17645a9b890d48972207a0
Shinichi Umegane <sumegane@nifty.com>
2023-08-30 19:08:55 +0900

    Remove unintended '@Tag("oracle")' from test cases
    Removed the '@Tag("oracle")' annotation which shouldn't have been
    present in certain test cases.

--- src/src/test/java/com/tsurugidb/benchmark/phonebill/db/postgresql/dao/DdlPostgresqlTest.java
+++ src/src/test/java/com/tsurugidb/benchmark/phonebill/db/postgresql/dao/DdlPostgresqlTest.java
@@ -6,7 +6,6 @@ import java.sql.Connection;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import com.tsurugidb.benchmark.phonebill.app.Config;
@@ -15,47 +14,46 @@ import com.tsurugidb.benchmark.phonebill.db.dao.Ddl;
 import com.tsurugidb.benchmark.phonebill.db.jdbc.PhoneBillDbManagerJdbc;
 
 class DdlPostgresqlTest {
-	private static PhoneBillDbManager manager;
-	private static Connection conn;
+    private static PhoneBillDbManager manager;
+    private static Connection conn;
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		manager = PhoneBillDbManager.createPhoneBillDbManager(Config.getConfig());
-		conn = ((PhoneBillDbManagerJdbc)manager).getConnection();
-		conn.setAutoCommit(false);
-	}
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        manager = PhoneBillDbManager.createPhoneBillDbManager(Config.getConfig());
+        conn = ((PhoneBillDbManagerJdbc)manager).getConnection();
+        conn.setAutoCommit(false);
+    }
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-		conn.close();
-		manager.close();
-	}
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        conn.close();
+        manager.close();
+    }
 
-	@Test
-	@Tag("oracle")
-	final void testTableExists() {
-		Ddl ddl = manager.getDdl();
-		ddl.dropTables();
-		assertFalse(ddl.tableExists("history"));
-		assertFalse(ddl.tableExists("contracts"));
-		assertFalse(ddl.tableExists("billing"));
-		assertFalse(ddl.tableExists("BILLING"));
-		assertFalse(ddl.tableExists("biLLing"));
+    @Test
+    final void testTableExists() {
+        Ddl ddl = manager.getDdl();
+        ddl.dropTables();
+        assertFalse(ddl.tableExists("history"));
+        assertFalse(ddl.tableExists("contracts"));
+        assertFalse(ddl.tableExists("billing"));
+        assertFalse(ddl.tableExists("BILLING"));
+        assertFalse(ddl.tableExists("biLLing"));
 
-		ddl.createBillingTable();
-		assertFalse(ddl.tableExists("history"));
-		assertFalse(ddl.tableExists("contracts"));
-		assertTrue(ddl.tableExists("billing"));
+        ddl.createBillingTable();
+        assertFalse(ddl.tableExists("history"));
+        assertFalse(ddl.tableExists("contracts"));
+        assertTrue(ddl.tableExists("billing"));
 
-		ddl.createContractsTable();
-		assertFalse(ddl.tableExists("history"));
-		assertTrue(ddl.tableExists("contracts"));
-		assertTrue(ddl.tableExists("billing"));
+        ddl.createContractsTable();
+        assertFalse(ddl.tableExists("history"));
+        assertTrue(ddl.tableExists("contracts"));
+        assertTrue(ddl.tableExists("billing"));
 
-		ddl.createHistoryTable();
-		assertTrue(ddl.tableExists("history"));
-		assertTrue(ddl.tableExists("contracts"));
-		assertTrue(ddl.tableExists("billing"));
-	}
+        ddl.createHistoryTable();
+        assertTrue(ddl.tableExists("history"));
+        assertTrue(ddl.tableExists("contracts"));
+        assertTrue(ddl.tableExists("billing"));
+    }
 
 }
