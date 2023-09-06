f400b8b93f02ed55862e06b59b7e37b0527278a0
Shinichi Umegane <sumegane@nifty.com>
2023-09-06 17:59:06 +0900

    Remove unnecessary indexes

--- src/src/main/java/com/tsurugidb/benchmark/phonebill/db/postgresql/dao/DdlPostgresql.java
+++ src/src/main/java/com/tsurugidb/benchmark/phonebill/db/postgresql/dao/DdlPostgresql.java
@@ -11,114 +11,112 @@ import com.tsurugidb.benchmark.phonebill.db.jdbc.dao.DdlJdbc;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 public class DdlPostgresql extends DdlJdbc {
-	private Connection conn;
+    private Connection conn;
 
-	public DdlPostgresql(PhoneBillDbManagerJdbc manager) {
-		super(manager);
-		conn = manager.getConnection();
-	}
+    public DdlPostgresql(PhoneBillDbManagerJdbc manager) {
+        super(manager);
+        conn = manager.getConnection();
+    }
 
-	public void createHistoryTable() {
-		String create_table = "create table history ("
-				+ "caller_phone_number varchar(15) not null," 		// 発信者電話番号
-				+ "recipient_phone_number varchar(15) not null," 	// 受信者電話番号
-				+ "payment_category char(1) not null," 			// 料金区分
-				+ "start_time timestamp not null,"			 		// 通話開始時刻
-				+ "time_secs integer not null," 					// 通話時間(秒)
-				+ "charge integer," 								// 料金
-				+ "df integer not null" 							// 論理削除フラグ
-				+ ") with (fillfactor=80)";
-		execute(create_table);
-	}
+    public void createHistoryTable() {
+        String create_table = "create table history ("
+                + "caller_phone_number varchar(15) not null," 		// 発信者電話番号
+                + "recipient_phone_number varchar(15) not null," 	// 受信者電話番号
+                + "payment_category char(1) not null," 			// 料金区分
+                + "start_time timestamp not null,"			 		// 通話開始時刻
+                + "time_secs integer not null," 					// 通話時間(秒)
+                + "charge integer," 								// 料金
+                + "df integer not null" 							// 論理削除フラグ
+                + ") with (fillfactor=80)";
+        execute(create_table);
+    }
 
-	@Override
-	public void prepareLoadData() {
-			long startTime = System.currentTimeMillis();
-			execute("truncate table history");
-			execute("truncate table contracts");
-			commit();
+    @Override
+    public void prepareLoadData() {
+            long startTime = System.currentTimeMillis();
+            execute("truncate table history");
+            execute("truncate table contracts");
+            commit();
 
-			dropPrimaryKey("history", "history_pkey");
-			dropPrimaryKey("contracts", "contracts_pkey");
-			dropIndex("idx_df");
-			dropIndex("idx_st");
-			dropIndex("idx_rp");
-			commit();
-			long elapsedTime = System.currentTimeMillis() - startTime;
-			String format = "Truncate teable and drop indexies in %,.3f sec ";
-			LOG.info(String.format(format, elapsedTime / 1000d));
-	}
+            dropPrimaryKey("history", "history_pkey");
+            dropPrimaryKey("contracts", "contracts_pkey");
+            dropIndex("idx_st");
+            dropIndex("idx_rp");
+            commit();
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            String format = "Truncate teable and drop indexies in %,.3f sec ";
+            LOG.info(String.format(format, elapsedTime / 1000d));
+    }
 
-	@SuppressFBWarnings("SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE")
-	@Override
-	public void createIndexes() {
-		createIndexes(true);
-	}
+    @SuppressFBWarnings("SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE")
+    @Override
+    public void createIndexes() {
+        createIndexes(true);
+    }
 
-	protected void createIndexes(boolean createSecondaryIndexes) {
-		long startTime = System.currentTimeMillis();
-		if (createSecondaryIndexes) {
-			executeWithLogging("create index idx_df on history(df)");
-			executeWithLogging("create index idx_st on history(start_time)");
-			executeWithLogging("create index idx_rp on history(recipient_phone_number, payment_category, start_time)");
-		}
-		executeWithLogging(
-				"alter table history add constraint history_pkey primary key (caller_phone_number, payment_category, start_time)");
-		executeWithLogging(
-				"alter table contracts add constraint contracts_pkey primary key (phone_number, start_date)");
-		long elapsedTime = System.currentTimeMillis() - startTime;
-		String format = "Create indexies in %,.3f sec ";
-		LOG.info(String.format(format, elapsedTime / 1000d));
-	}
+    protected void createIndexes(boolean createSecondaryIndexes) {
+        long startTime = System.currentTimeMillis();
+        if (createSecondaryIndexes) {
+            executeWithLogging("create index idx_st on history(start_time)");
+            executeWithLogging("create index idx_rp on history(recipient_phone_number, payment_category, start_time)");
+        }
+        executeWithLogging(
+                "alter table history add constraint history_pkey primary key (caller_phone_number, payment_category, start_time)");
+        executeWithLogging(
+                "alter table contracts add constraint contracts_pkey primary key (phone_number, start_date)");
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        String format = "Create indexies in %,.3f sec ";
+        LOG.info(String.format(format, elapsedTime / 1000d));
+    }
 
 
-	/**
-	 * 指定のテーブルからPrimaryKeyを削除する
-	 *
-	 * @param table
-	 * @param pk
-	 */
-	@SuppressFBWarnings("SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE")
-	private void dropPrimaryKey(String table, String pk) {
-		execute("alter table " + table + " drop constraint if exists " + pk);
-	}
+    /**
+     * 指定のテーブルからPrimaryKeyを削除する
+     *
+     * @param table
+     * @param pk
+     */
+    @SuppressFBWarnings("SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE")
+    private void dropPrimaryKey(String table, String pk) {
+        execute("alter table " + table + " drop constraint if exists " + pk);
+    }
 
-	/**
-	 * 指定のテーブルから指定のIndexを削除する
-	 *
-	 * @param index
-	 */
-	@SuppressFBWarnings("SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE")
-	protected void dropIndex(String index) {
-		execute("drop index if exists " + index);
-	}
+    /**
+     * 指定のテーブルから指定のIndexを削除する
+     *
+     * @param index
+     */
+    @SuppressFBWarnings("SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE")
+    protected void dropIndex(String index) {
+        execute("drop index if exists " + index);
+    }
 
-	@SuppressFBWarnings("SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE")
-	@Override
-	public void updateStatistics() {
-		long startTime = System.currentTimeMillis();
-		execute("analyze history");
-		execute("analyze contracts");
-		long elapsedTime = System.currentTimeMillis() - startTime;
-		String format = "Update statistic in %,.3f sec ";
-		LOG.info(String.format(format, elapsedTime / 1000d));
-	}
+    @SuppressFBWarnings("SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE")
+    @Override
+    public void updateStatistics() {
+        long startTime = System.currentTimeMillis();
+        execute("analyze history");
+        execute("analyze contracts");
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        String format = "Update statistic in %,.3f sec ";
+        LOG.info(String.format(format, elapsedTime / 1000d));
+    }
 
-	@Override
-	public void dropTable(String tableName) {
-		execute("drop table if exists " + tableName);
-	}
+    @Override
+    public void dropTable(String tableName) {
+        execute("drop table if exists " + tableName);
+    }
 
-	@Override
-	public boolean tableExists(String tableName) {
-		String sql = "SELECT table_name FROM information_schema.tables WHERE table_name = ?";
-		try (PreparedStatement ps = conn.prepareStatement(sql)) {
-			ps.setString(1, tableName);
-			try (ResultSet rs = ps.executeQuery()) {
-				return rs.next();
-			}
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    @Override
+    public boolean tableExists(String tableName) {
+        String sql = "SELECT table_name FROM information_schema.tables WHERE table_name = ?";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, tableName);
+            try (ResultSet rs = ps.executeQuery()) {
+                return rs.next();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/src/test/java/com/tsurugidb/benchmark/phonebill/app/CreateTableTest.java
+++ src/src/test/java/com/tsurugidb/benchmark/phonebill/app/CreateTableTest.java
@@ -24,189 +24,186 @@ import com.tsurugidb.benchmark.phonebill.db.dao.Ddl;
 import com.tsurugidb.benchmark.phonebill.db.jdbc.PhoneBillDbManagerJdbc;
 
 class CreateTableTest extends AbstractJdbcTestCase {
-	private static final String ORACLE_CONFIG_PATH = "src/test/config/oracle.properties";
+    private static final String ORACLE_CONFIG_PATH = "src/test/config/oracle.properties";
 
-	@Test
-	void test() throws SQLException, IOException {
-		Config config = Config.getConfig();
-		try (PhoneBillDbManager manager = PhoneBillDbManager.createPhoneBillDbManager(config)) {
-			Ddl ddl = manager.getDdl();
+    @Test
+    void test() throws SQLException, IOException {
+        Config config = Config.getConfig();
+        try (PhoneBillDbManager manager = PhoneBillDbManager.createPhoneBillDbManager(config)) {
+            Ddl ddl = manager.getDdl();
 
-			ddl.dropTables();
-			// テーブルが存在しないことを確認
-			assertFalse(existsTable("billing"));
-			assertFalse(existsTable("contracts"));
-			assertFalse(existsTable("history"));
+            ddl.dropTables();
+            // テーブルが存在しないことを確認
+            assertFalse(existsTable("billing"));
+            assertFalse(existsTable("contracts"));
+            assertFalse(existsTable("history"));
 
-			// テーブルが作成されることを確認
-			ddl.createBillingTable();
-			assertTrue(existsTable("billing"));
-			ddl.createContractsTable();
-			assertTrue(existsTable("contracts"));
-			ddl.createHistoryTable();
-			assertTrue(existsTable("history"));
-		}
-	}
+            // テーブルが作成されることを確認
+            ddl.createBillingTable();
+            assertTrue(existsTable("billing"));
+            ddl.createContractsTable();
+            assertTrue(existsTable("contracts"));
+            ddl.createHistoryTable();
+            assertTrue(existsTable("history"));
+        }
+    }
 
-	@Test
-	void testExecute() throws Exception {
-		CreateTable createTable = new CreateTable();
-		createTable.execute(Config.getConfig());
+    @Test
+    void testExecute() throws Exception {
+        CreateTable createTable = new CreateTable();
+        createTable.execute(Config.getConfig());
 
-		// テーブルが作成されることを確認
-		assertTrue(existsTable("billing"));
-		assertTrue(existsTable("contracts"));
-		assertTrue(existsTable("history"));
-	}
-
-
-	/**
-	 * prepareLoadData()とafterLoadData()のテスト
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	void testPrepareAndAfterLoadData() throws Exception {
-		testPrepareAndAfterLoadDataSub(Config.getConfig());
-	}
-
-	/**
-	 * prepareLoadData()とafterLoadData()のテスト(oracle)
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	@Tag("oracle")
-	void testPrepareAndAfterLoadDataOracle() throws Exception {
-		testPrepareAndAfterLoadDataSub(Config.getConfig(ORACLE_CONFIG_PATH));
-	}
+        // テーブルが作成されることを確認
+        assertTrue(existsTable("billing"));
+        assertTrue(existsTable("contracts"));
+        assertTrue(existsTable("history"));
+    }
 
 
+    /**
+     * prepareLoadData()とafterLoadData()のテスト
+     *
+     * @throws Exception
+     */
+    @Test
+    void testPrepareAndAfterLoadData() throws Exception {
+        testPrepareAndAfterLoadDataSub(Config.getConfig());
+    }
 
-	private void testPrepareAndAfterLoadDataSub(Config config) throws Exception {
-		try (PhoneBillDbManagerJdbc manager = (PhoneBillDbManagerJdbc) PhoneBillDbManager
-				.createPhoneBillDbManager(config, SessionHoldingType.INSTANCE_FIELD)) {
-			Connection conn = manager.getConnection();
-			// テーブルを作成
-			CreateTable createTable = new CreateTable();
-			createTable.execute(config);
+    /**
+     * prepareLoadData()とafterLoadData()のテスト(oracle)
+     *
+     * @throws Exception
+     */
+    @Test
+    @Tag("oracle")
+    void testPrepareAndAfterLoadDataOracle() throws Exception {
+        testPrepareAndAfterLoadDataSub(Config.getConfig(ORACLE_CONFIG_PATH));
+    }
 
-			String history = config.dbmsType == DbmsType.ORACLE_JDBC ? "HISTORY" : "history";
-			String contracts = config.dbmsType == DbmsType.ORACLE_JDBC ? "CONTRACTS" : "contracts";
 
-			Set<String> historyIndexSet = new HashSet<String>();
-			historyIndexSet.add("history_pkey");
-			historyIndexSet.add("idx_df");
-			historyIndexSet.add("idx_st");
-			historyIndexSet.add("idx_rp");
 
-			Set<String> contractsIndexSet = new HashSet<String>();
-			contractsIndexSet.add("contracts_pkey");
+    private void testPrepareAndAfterLoadDataSub(Config config) throws Exception {
+        try (PhoneBillDbManagerJdbc manager = (PhoneBillDbManagerJdbc) PhoneBillDbManager
+                .createPhoneBillDbManager(config, SessionHoldingType.INSTANCE_FIELD)) {
+            Connection conn = manager.getConnection();
+            // テーブルを作成
+            CreateTable createTable = new CreateTable();
+            createTable.execute(config);
 
-			// インデックスの存在を確認
-			assertEquals(historyIndexSet, getIndexNameSet(conn, history));
-			assertEquals(contractsIndexSet, getIndexNameSet(conn, contracts));
+            String history = config.dbmsType == DbmsType.ORACLE_JDBC ? "HISTORY" : "history";
+            String contracts = config.dbmsType == DbmsType.ORACLE_JDBC ? "CONTRACTS" : "contracts";
 
-			// インデックス削除されたことを確認
-			Ddl ddl = manager.getDdl();
-			ddl.prepareLoadData();
-			assertEquals(Collections.EMPTY_SET, getIndexNameSet(conn, history));
-			assertEquals(Collections.EMPTY_SET, getIndexNameSet(conn, contracts));
+            Set<String> historyIndexSet = new HashSet<String>();
+            historyIndexSet.add("history_pkey");
+            historyIndexSet.add("idx_st");
+            historyIndexSet.add("idx_rp");
 
-			// インデックスが復活したことを確認
-			ddl.afterLoadData();
-			assertEquals(historyIndexSet, getIndexNameSet(conn, history));
-			assertEquals(contractsIndexSet, getIndexNameSet(conn, contracts));
-		}
-	}
+            Set<String> contractsIndexSet = new HashSet<String>();
+            contractsIndexSet.add("contracts_pkey");
 
-	/**
-	 * dropIndex()とdropPrimaryKey()のテスト
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	void testDropIndexAndDropPrimaryKey() throws Exception {
-		testDropIndexAndDropPrimaryKeySub(Config.getConfig());
-	}
+            // インデックスの存在を確認
+            assertEquals(historyIndexSet, getIndexNameSet(conn, history));
+            assertEquals(contractsIndexSet, getIndexNameSet(conn, contracts));
 
-	/**
-	 * dropIndex()とdropPrimaryKey()のテスト(oracle)
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	@Tag("oracle")
-	void testDropIndexAndDropPrimaryKeyOracle() throws Exception {
-		testDropIndexAndDropPrimaryKeySub(Config.getConfig(ORACLE_CONFIG_PATH));
-	}
+            // インデックス削除されたことを確認
+            Ddl ddl = manager.getDdl();
+            ddl.prepareLoadData();
+            assertEquals(Collections.EMPTY_SET, getIndexNameSet(conn, history));
+            assertEquals(Collections.EMPTY_SET, getIndexNameSet(conn, contracts));
 
-	void testDropIndexAndDropPrimaryKeySub(Config config) throws Exception {
-		try (PhoneBillDbManagerJdbc manager = (PhoneBillDbManagerJdbc) PhoneBillDbManager
-				.createPhoneBillDbManager(config, SessionHoldingType.INSTANCE_FIELD)) {
-			Connection conn = manager.getConnection();
-			// テーブルを作成
-			CreateTable createTable = new CreateTable();
-			createTable.execute(config);
-			Ddl ddl = manager.getDdl();
+            // インデックスが復活したことを確認
+            ddl.afterLoadData();
+            assertEquals(historyIndexSet, getIndexNameSet(conn, history));
+            assertEquals(contractsIndexSet, getIndexNameSet(conn, contracts));
+        }
+    }
 
-			String tableName = config.dbmsType == DbmsType.ORACLE_JDBC ? "HISTORY" : "history";
+    /**
+     * dropIndex()とdropPrimaryKey()のテスト
+     *
+     * @throws Exception
+     */
+    @Test
+    void testDropIndexAndDropPrimaryKey() throws Exception {
+        testDropIndexAndDropPrimaryKeySub(Config.getConfig());
+    }
 
-			// インデックスの存在を確認
-			assertTrue(getIndexNameSet(conn, tableName).contains("idx_df"));
-			assertTrue(getIndexNameSet(conn, tableName).contains("history_pkey"));
+    /**
+     * dropIndex()とdropPrimaryKey()のテスト(oracle)
+     *
+     * @throws Exception
+     */
+    @Test
+    @Tag("oracle")
+    void testDropIndexAndDropPrimaryKeyOracle() throws Exception {
+        testDropIndexAndDropPrimaryKeySub(Config.getConfig(ORACLE_CONFIG_PATH));
+    }
 
-			// インデックス、PK削除
-			ddl.prepareLoadData();
-			assertFalse(getIndexNameSet(conn, tableName).contains("idx_df"));
-			assertFalse(getIndexNameSet(conn, tableName).contains("history_pkey"));
+    void testDropIndexAndDropPrimaryKeySub(Config config) throws Exception {
+        try (PhoneBillDbManagerJdbc manager = (PhoneBillDbManagerJdbc) PhoneBillDbManager
+                .createPhoneBillDbManager(config, SessionHoldingType.INSTANCE_FIELD)) {
+            Connection conn = manager.getConnection();
+            // テーブルを作成
+            CreateTable createTable = new CreateTable();
+            createTable.execute(config);
+            Ddl ddl = manager.getDdl();
 
-			// 2回呼んでもエラーにならない
-			ddl.prepareLoadData();
-			assertFalse(getIndexNameSet(conn, tableName).contains("idx_df"));
-			assertFalse(getIndexNameSet(conn, tableName).contains("history_pkey"));
-		}
-	}
+            String tableName = config.dbmsType == DbmsType.ORACLE_JDBC ? "HISTORY" : "history";
 
-	/**
-	 * 指定のテーブルに存在するインデックスのセットを返す
-	 *
-	 * @param indexname
-	 * @return 存在するときtrue
-	 * @throws SQLException
-	 */
-	private Set<String> getIndexNameSet(Connection conn, String table) throws SQLException {
-		Set<String> set = new HashSet<>();
-		DatabaseMetaData metaData = conn.getMetaData();
-		try (ResultSet rs = metaData.getIndexInfo(null, null, table, false, true)) {
-			while (rs.next()) {
-				String str = rs.getString("INDEX_NAME");
-				if(str != null) {
-					set.add(str.toLowerCase(Locale.ROOT));  // Oracleの場合インデックス名が大文字になってしまうので小文字に戻す
-				}
-			}
-		}
-		return set;
-	}
+            // インデックスの存在を確認
+            assertTrue(getIndexNameSet(conn, tableName).contains("history_pkey"));
 
-	/**
-	 * 指定した名称のテーブルが存在するかチェックする
-	 *
-	 * @param tablename
-	 * @return 存在するときtrue
-	 * @throws SQLException
-	 */
-	private boolean existsTable(String tablename) throws SQLException {
-		String sql = "SELECT count(*) from pg_class WHERE relkind = 'r' AND relname = ?";
-		int c = 0;
-		try (PreparedStatement ps = getConn().prepareStatement(sql)) {
-			ps.setString(1, tablename);
-			try (ResultSet rs = ps.executeQuery()) {
-				if (rs.next()) {
-					c = rs.getInt(1);
-				}
-			}
-		}
-		return c == 1;
-	}
+            // インデックス、PK削除
+            ddl.prepareLoadData();
+            assertFalse(getIndexNameSet(conn, tableName).contains("history_pkey"));
+
+            // 2回呼んでもエラーにならない
+            ddl.prepareLoadData();
+            assertFalse(getIndexNameSet(conn, tableName).contains("idx_df"));
+            assertFalse(getIndexNameSet(conn, tableName).contains("history_pkey"));
+        }
+    }
+
+    /**
+     * 指定のテーブルに存在するインデックスのセットを返す
+     *
+     * @param indexname
+     * @return 存在するときtrue
+     * @throws SQLException
+     */
+    private Set<String> getIndexNameSet(Connection conn, String table) throws SQLException {
+        Set<String> set = new HashSet<>();
+        DatabaseMetaData metaData = conn.getMetaData();
+        try (ResultSet rs = metaData.getIndexInfo(null, null, table, false, true)) {
+            while (rs.next()) {
+                String str = rs.getString("INDEX_NAME");
+                if(str != null) {
+                    set.add(str.toLowerCase(Locale.ROOT));  // Oracleの場合インデックス名が大文字になってしまうので小文字に戻す
+                }
+            }
+        }
+        return set;
+    }
+
+    /**
+     * 指定した名称のテーブルが存在するかチェックする
+     *
+     * @param tablename
+     * @return 存在するときtrue
+     * @throws SQLException
+     */
+    private boolean existsTable(String tablename) throws SQLException {
+        String sql = "SELECT count(*) from pg_class WHERE relkind = 'r' AND relname = ?";
+        int c = 0;
+        try (PreparedStatement ps = getConn().prepareStatement(sql)) {
+            ps.setString(1, tablename);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    c = rs.getInt(1);
+                }
+            }
+        }
+        return c == 1;
+    }
 }
