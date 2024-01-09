f19246171b0929bb1859ea76136ebd72600d5ac5
Shinichi Umegane <sumegane@nifty.com>
2024-01-09 18:45:51 +0900

    Remove TSURUGI_HOME environment variable check from shell scripts

--- src/src/dist/bin/create_table.sh
+++ src/src/dist/bin/create_table.sh
@@ -5,10 +5,6 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-if [[ -z "$TSURUGI_HOME" ]]; then
-  echo "Error: The environment variable TSURUGI_HOME is not set. Please set TSURUGI_HOME."
-fi
-
 BIN_DIR=$(cd $(dirname $0); pwd)
 
 ext=`date "+%Y%m%d-%H%M%S"`.log
